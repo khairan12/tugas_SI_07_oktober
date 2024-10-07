226bf05dc6ddb02d305a6ed4ae3c83f624dd86e7
khairan15 <90371437+khairan12@users.noreply.github.com>
2024-10-07 14:57:38 +0700

    Add files via upload

---
+++ pertambahanpy/perkalian.py
@@ -0,0 +1,9 @@
+# mendifinesikan dua angka 
+angka1 = 5
+angka2 = 5
+
+# menghitung perkalian 
+hasil = angka1 * angka2
+
+# menampilkan hasil
+print("Hasil perkalian:", hasil)
---
+++ pertambahanpy/pertambahan.py
@@ -0,0 +1,9 @@
+# program pertambahan dua angka tanpa input
+# mendefinisikan dua angka 
+angka1 = int(input ("masukkan angka pertama:"))
+angka2 = int(input ("masukkan angka kedua::"))
+
+#menambahkan angka 
+hasil = angka1 + angka2
+#menampilkan hasil pertambahan 
+print("Hasil pertambahan:", hasil)
